b49b92f75514ccd1a159a9395820dbf5bb632063
Juanscorr21 <juancs1921@gmail.com>
2021-08-19 15:23:36 -0500

    base de datos h2

--- src/test/java/com/prueba/PruebaJavaApplicationTests.java
+++ src/test/java/com/prueba/PruebaJavaApplicationTests.java
@@ -3,6 +3,7 @@ package com.prueba;
 
 import static org.junit.Assert.assertTrue;
 
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
@@ -16,21 +17,4 @@ import com.prueba.service.UserService;
 @SpringBootTest
 class PruebaJavaApplicationTests {
 	
-	
-	@Mock
-	UserService userService;
-	
-	@Test
-	public void testUser(){	
-		User user = new User();
-	 	user.setId((long) 19);
-		user.setName("camila");
-		user.setLastname("restrepo");
-		user.setPhone("132435");
-		user.setUserName("res123");
-		user.setPassword("Rest123");	
-		
-		
-		assertTrue("Nombre usuario es null", user.getName() != null);	
-	}	
 }
